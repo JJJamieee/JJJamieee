8453f8771ff3747b3d5d179a3a172dcbefac7c0d
JJJamieee <45135781+JJJamieee@users.noreply.github.com>
2020-04-28 23:44:14 +0800

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -1,37 +1,24 @@
-var cnt = 1
-var changePhoto = () => {
-    var person1 = $('#photo1')
-    var person2 = $('#photo2')
-    cnt %= 2
-    if (cnt == 0) {
-        person1.attr('src', './img/person1.jpg')
-        person2.attr('src', './img/person2.png').attr('class', 'p21')
-    } else {
-        person2.attr('src', './img/person1.jpg').attr('class', 'p12')
-        person1.attr('src', './img/person2.png')
-    }
-    cnt++
-}
-
 $(() => {
-    setInterval(changePhoto, 3000)
+    var $card = $('.card');
 
-    $('.card').hover(
+    $card.hover(
         function() {
-            $(this).addClass("card-hover");
+            $(this).addClass("card-hover-color");
+            $(this).find('img').addClass("card-img-hover");
         },
         function() {
-            $(this).removeClass("card-hover");
+            $(this).removeClass("card-hover-color");
+            $(this).find('img').removeClass("card-img-hover");
         }
     );
 
-    $(window).scroll(function() {
+    $(window).scroll(() => {
         var win_h = $(document).scrollTop();
         if (win_h >= ($('#nav').height() * 4)) {
             $('#nav').removeClass('nav-color1').addClass('sticky-top').addClass('nav-color2');
         } else {
             $('#nav').removeClass('nav-color2').removeClass('sticky-top').addClass('nav-color1');
-        }
+        };
     });
 
-})
+});
